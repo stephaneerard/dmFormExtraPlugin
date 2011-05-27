e6705f3895ad8ede320955d60629cfec9a2ca0da
Stéphane Erard <stephane.erard@gmail.com>
2011-05-28 00:26:48 +0200

    Fix: was not working as expected

--- web/js/dmFormAutocomplete.js
+++ web/js/dmFormAutocomplete.js
@@ -5,15 +5,14 @@
 	
 
 	$.each(autocompleters, function(i, v){
-		console.log($('#' + v.id));
-		
 		    jQuery('#' + v.id)
 		    .autocomplete(v.url, jQuery.extend({}, {
 		      dataType: 'json',
 		      parse:    function(data) {
 		        var parsed = [];
-		        for (key in data) {
-		          parsed[parsed.length] = { data: [ data[key], key ], value: data[key], result: data[key] };
+		        for (var key in data) {
+		          if(data.hasOwnProperty(key) && data[key].id)
+		          parsed[parsed.length] = { data: [ data[key].value, data[key].id], value: data[key].value, result: data[key].value };
 		        }
 		        return parsed;
 		      }
